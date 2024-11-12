a096639ade88e57f8cf71094fbb4a35e69c79a4d
Dany3gs <dsmmgc@gmail.com>
2024-11-12 23:43:04 +0000

    ejercicio 16 terminado, arrays

--- Asignaturas/LND/JavaScript/3.1 actividadesJsExamen/ejercicio16/ejercicio16.js
+++ Asignaturas/LND/JavaScript/3.1 actividadesJsExamen/ejercicio16/ejercicio16.js
@@ -2,33 +2,39 @@
 
 let numero1 = parseFloat(prompt("Indique un número para comparar los divisibles con otro número."));
 let numero2 = parseFloat(prompt("Escriba otro número"));
-let divisores = ""; // Variable para almacenar los múltiplos
 
+let divisores1 = [];
+let divisores2 = [];
+let divisoresComunes = [];
 
-    for (let i = 0; i <= numero1; i++) {
-        if (numero1 === 1) {
-            continue;
-        }
+//obtenemos los divisores de numero1
+  for (let i = 1; i <= numero1; i++) {
         if (numero1 % i === 0 ) {
-            divisores += (i + ", "); 
-        }
-    }
-    for (let i = 0; i <= numero2; i++) {
-        if (numero2 === 1) {
-            continue;
-        }
-        if (numero2 % i === 0 ) {
-            divisores += (i + ", "); 
+            divisores1.push(i); // con push añadimos i al final del array
         }
     }
 
-if (divisores) {
-    document.write("El número " + numero + " es múltiplo de: " + divisores.trim()); // trim es para quitar espacios al final y al principio
-} else {
-    document.write("El número no tiene divisores");
-}
+//obtenemos los divisores de numero2  
+   for (let i = 0; i <= numero2; i++) {
+        if (numero2 % i === 0) {
+            divisores2.push(i);
+        }
+    }
+//con un forof recorremos divisores1, en cada iteración el valor actual de divisoeres1 se asigna a divisor 
+    for (let divisor of divisores1) {
+        if (divisores2.includes(divisor)) { //Con .includes(divisor) comprobamos si divisores2 contiene el valor divisor. Si es así, significa que el divisor es común a ambos números.
+            divisoresComunes.push(divisor); //Si divisor se encuentra en ambos arrays (divisores1 y 2) se agrega a divisoresComunes con push().
+        }
+    }
+  
+document.write("El número " + numero1 + " tienen como divisores: " + divisores1.join(", ") + "<br>"); // con .join() separamos los elementos del array, idicando como queremos la separación ", "
+document.write("El número " + numero2 + " tienen como divisores: " + divisores2.join(", ") + "<br>"); 
+document.write("Los divisores en común son " + divisoresComunes.join(", ") + "<br>"); 
 
 
-//pdt
+
+
+
+
 
 
